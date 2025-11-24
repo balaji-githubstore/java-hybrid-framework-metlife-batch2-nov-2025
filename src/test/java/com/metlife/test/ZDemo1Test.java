b96fd48fbc7667432f2d3d9828c58749eaa40892
balaji-githubstore <balajidinakaran1@gmail.com>
2025-11-24 15:05:57 +0530

    Added ExcelUtils.java

--- src/test/java/com/metlife/test/ZDemo1Test.java
+++ src/test/java/com/metlife/test/ZDemo1Test.java
@@ -9,18 +9,18 @@ import org.testng.annotations.Test;
 public class ZDemo1Test {
 
     @DataProvider
-    public String[][] getValidData() {
-        String[][] arr=new String[3][2];
+    public Object[][] getValidData() {
+        Object[][] arr=new Object[3][2];
         //i--> number of testcase
         //j-->number of parameters
         arr[0][0]="john";
-        arr[0][1]="john123";
+        arr[0][1]=787;
 
         arr[1][0]="kim";
-        arr[1][1]="kim123";
+        arr[1][1]=77;
 
         arr[2][0]="saul";
-        arr[2][1]="saul123";
+        arr[2][1]=88;
 
         return arr;
     }
@@ -28,7 +28,7 @@ public class ZDemo1Test {
 
 
     @Test(dataProvider = "getValidData")
-    public void validTest(String username, String password)
+    public void validTest(String username, int password)
     {
         System.out.println("test login"+username+"----"+password);
     }
---
+++ src/test/java/com/metlife/utilities/ExcelUtils.java
@@ -0,0 +1,39 @@
+package com.metlife.utilities;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ExcelUtils {
+
+    /**
+     * Converts sheet into two-dimensional Object[][]
+     * @param filePath
+     * @param sheetname
+     * @return Object[][]
+     * @throws IOException
+     */
+    public static Object[][] getSheetIntoTwoDimensionalArray(String filePath, String sheetname) throws IOException {
+        FileInputStream file = new FileInputStream(filePath);
+        XSSFWorkbook book = new XSSFWorkbook(file);
+        XSSFSheet sheet = book.getSheet(sheetname);
+
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
+
+        Object[][] arr = new Object[rowCount - 1][cellCount];
+
+        DataFormatter formatter = new DataFormatter();
+        for (int r = 1; r < rowCount; r++) {
+            for (int c = 0; c < cellCount; c++) {
+                arr[r - 1][c] = formatter.formatCellValue(sheet.getRow(r).getCell(c));
+            }
+        }
+        book.close();
+        file.close();
+        return arr;
+    }
+}
